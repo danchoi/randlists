1f86732815dd96138f344b80a9fa61ace0fd9a91
Daniel Choi <dhchoi@gmail.com>
2016-05-01 09:24:06 -0400

    initial commit

---
+++ Main.hs
@@ -0,0 +1,52 @@
+{-# LANGUAGE OverloadedStrings, RecordWildCards, ScopedTypeVariables #-} 
+module Main where
+import System.Environment
+import System.Random
+import Control.Monad.State
+import Options.Applicative
+import Control.Applicative
+import Data.Text (Text)
+import qualified Data.Text as T 
+import qualified Data.Text.IO as T 
+
+options :: Parser (Int, [FilePath])
+options = (,)
+    <$> option auto (
+           short  'n' 
+        <> metavar "ROWS"
+        <> help "Output n rows. Default is to output the number of rows in the longest file."
+        <> value 0)
+    <*> many1 (
+          strArgument ( metavar "FILEPATH" <> help "File with content to randomize")
+        )
+
+many1 :: Parser a -> Parser [a]
+many1 p = (:) <$> p <*> (many p)
+
+opts = info (helper <*> options) 
+            (header "randlists" <> fullDesc
+            <> progDesc "Generates random lists"
+            <> footer "https://github.com/danchoi/randlists")
+
+getRandomElem :: [Text] -> State StdGen Text
+getRandomElem xs = do
+  gen <- get 
+  let (idx, gen') = randomR (0, length xs - 1) gen 
+  put gen' 
+  return (xs !! idx)
+
+getRandomElems :: [[Text]] -> State StdGen [Text]
+getRandomElems xxs = mapM getRandomElem xxs
+
+  
+main = do
+  (n', files) <- execParser opts
+  xxs :: [[Text]] <- mapM (\file -> T.lines `fmap` T.readFile file) files
+  let n = if n' < 1
+          then maximum $ map length xxs
+          else n'
+  g <- getStdGen 
+  let r :: [[Text]]
+      r = evalState (replicateM n $ getRandomElems xxs) g
+  mapM (T.putStrLn . T.unwords) r
+
---
+++ README.md
@@ -0,0 +1,12 @@
+# randlists
+
+Outputs random combinations of multiple lists.  
+
+Each file argument must be a simple list items (strings or ints).
+
+
+   randlists  
+
+
+If no limit is given, it will generate rows infinitely, until the pipeline is closed. 
+
